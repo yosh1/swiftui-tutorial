332f565e153ade16f483163cf760eff5012f6a12
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-07-14 11:55:29 +0900

    Initial Commit

---
+++ first-swiftuiTests/first_swiftuiTests.swift
@@ -0,0 +1,34 @@
+//
+//  first_swiftuiTests.swift
+//  first-swiftuiTests
+//
+//  Created by Yoshihisa Kaino on 2019/07/14.
+//  Copyright © 2019 Yoshihisa Kaino. All rights reserved.
+//
+
+import XCTest
+@testable import first_swiftui
+
+class first_swiftuiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
